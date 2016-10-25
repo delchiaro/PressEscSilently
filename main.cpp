6df003f117e2bc9c4edc98e306b7443bdec19479
Nagash <nagash91@gmail.com>
2016-10-25 20:51:29 +0200

    Commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+#pragma comment(linker, "/SUBSYSTEM:WINDOWS")
+#define WINVER 0x0500
+#include <windows.h>
+
+
+
+void pressESC() {
+    INPUT ip;
+    // Set up a generic keyboard event.
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0; // hardware scan code for key
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
+
+    // Press the "A" key
+    ip.ki.wVk = 0x1B; // ESCAPE KEY IN WINDOWS SPECIAL KEYS
+    ip.ki.dwFlags = 0; // 0 for key press
+    SendInput(1, &ip, sizeof(INPUT));
+
+    // Release the "A" key
+    ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+    SendInput(1, &ip, sizeof(INPUT));
+
+}
+
+
+int main()
+{
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow( hWnd, SW_MINIMIZE );  //won't hide the window without SW_MINIMIZE
+    ShowWindow( hWnd, SW_HIDE );
+    pressESC();
+    return 0;
+}
